af7b1cb3bfa78425650f6307077417f9e76daa86
miikepine <miikepiine@gmail.com>
2024-01-22 12:18:14 +0100

    login issue

--- server/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    client_id: 'pinetree-e4ca85',
-    client_secret: '6c8a139d-0a6a-4741-ab8d-9b761ded0d88',
-    redirect_uri: 'http://localhost:5173/Banking',
-  };
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,6 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import { createClient } from '@supabase/supabase-js';
-
-
-const supabase = createClient(
-  'https://tubnpuzyuhjyhslwbshd.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1Ym5wdXp5dWhqeWhzbHdic2hkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI4NTc1OTEsImV4cCI6MjAwODQzMzU5MX0.G-Z3MGdzPiDCEpa_vOpriMvivCrlyBInVuf8z1COOxQ'
-  );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
