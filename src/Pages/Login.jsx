842ed5bbca6fe193359d72f7f28109b1d1173a0d
miikepine <miikepiine@gmail.com>
2024-02-02 00:28:25 +0100

    minor changes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,79 +1,4 @@
-// import React, { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import auroras from "../../Images/pineWebp.webp";
-// import logo from "../../Images/logo2.png";
-// import supabase from "../../supabase/supabase";
-// import { Auth } from '@supabase/auth-ui-react'
-// import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-// const Login = () => {
-//   const [loading, setLoading] = useState(false);
-//   const navigate = useNavigate();
-  
-//   useEffect(() => {
-//     const authListener = supabase.auth.onAuthStateChange(async (event, session) => {
-//       if (event === "SIGNED_IN") {
-//         const returnUrl = localStorage.getItem('returnUrl');
-//         if (returnUrl) {
-//           navigate(returnUrl);
-//           localStorage.removeItem('returnUrl');
-//         } else {
-//           navigate("/Dashboard");
-//         }
-//       } else {
-//         navigate("/");
-//       }
-//     });    
-
-//     // Retorna uma função de limpeza para desinscrever o ouvinte ao desmontar o componente
-//     return () => {
-//       authListener.data.unsubscribe(); // Desinscreve o ouvinte de autenticação
-//     };
-//   }, []);
-
-//   const handleLoginClick = () => {
-//     localStorage.setItem('returnUrl', window.location.pathname);
-//     navigate("/Login");
-//   };
-
-//   return (
-//     <div
-//       className="min-h-screen flex items-center justify-center bg-cover bg-no-repeat backdrop-blur-sm"
-//       style={{ backgroundImage: `url(${auroras})` }}
-//     >
-//       <div className="w-full md:w-1/3 bg-zinc-200 py-4 px-8 pt-10">
-//         <div className="">
-//           <img src={logo} alt="Logo" className="h-32 md:h-36 mt-[-30px] ml-[-10px]" />
-//           <div className="text-teal-700 text-md md:text-2xl">Login</div>
-//         </div>
-//         <p className="text-gray-500 text-xs md:text-sm">Sign in for your expenses</p>
-//         <Auth
-//           supabaseClient={supabase}
-//           theme="default"
-//           providers={['google', 'linkedin', 'discord', 'github']}
-//           appearance={{
-//             theme: ThemeSupa,
-//             variables: {
-//               default: {
-//                 colors: {
-//                   brand: 'teal',
-//                   brandAccent: 'teal',
-//                   dividerBackground: 'teal',
-//                   inputBackground: 'white',
-//                 },
-//               },
-//             },
-//           }} 
-//           className="w-full md:w-1/2 text-xs md:text-xl"
-//           socialLayout="horizontal"
-//           onLogin={() => handleLoginClick()}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
 
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -90,36 +15,43 @@ const Login = () => {
   
   useEffect(() => {
     const listener = supabase.auth.onAuthStateChange(async (event, session) => {
+      console.log("Evento de mudança de autenticação:", event);
       if (event === "SIGNED_IN") {
         const returnUrl = localStorage.getItem('returnUrl');
         if (returnUrl) {
+          console.log("Redirecionando para:", returnUrl);
           navigate(returnUrl);
           localStorage.removeItem('returnUrl');
         } else {
+          console.log("Redirecionando para: /Dashboard");
           navigate("/Dashboard");
         }
       } else {
+        console.log("Redirecionando para: /");
         navigate("/");
       }
     });
-
+  
     setAuthListener(listener);
-
-    // Retorna uma função de limpeza para desinscrever o ouvinte ao desmontar o componente
+  
     return () => {
       if (authListener) {
-        authListener.data.unsubscribe(); // Desinscreve o ouvinte de autenticação
+        authListener.unsubscribe();
       }
     };
   }, []);
+  
+
+
 
   const handleLoginClick = async () => {
-    // Passando o redirectTo como uma opção para a função signIn
-    const { user, session, error } = await supabase.auth.signIn(
-      { provider: 'google' },
-      { redirectTo: '/Dashboard' } // Aqui você define o redirectTo como '/Dashboard'
-    );
-  };
+  console.log("Clicou no botão de login do Google");
+  const { user, session, error } = await supabase.auth.signIn(
+    { provider: 'google' },
+    { redirectTo: '/Dashboard' }
+  );
+  console.log("Resultado da tentativa de login:", { user, session, error });
+};
 
   return (
     <div
