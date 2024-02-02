0d5cd7d19b39e59c9a6db24084da6a86bb2124f9
miikepine <miikepiine@gmail.com>
2024-02-02 11:10:32 +0100

    login in google

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,4 +1,96 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import auroras from "../../Images/pineWebp.webp";
+// import logo from "../../Images/logo2.png";
+// import supabase from "../../supabase/supabase";
+// import { Auth } from '@supabase/auth-ui-react'
+// import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+// const Login = () => {
+//   const [loading, setLoading] = useState(false);
+//   const navigate = useNavigate();
+//   const [authListener, setAuthListener] = useState(null);
+  
+//   useEffect(() => {
+//     const listener = supabase.auth.onAuthStateChange(async (event, session) => {
+//       console.log("Evento de mudança de autenticação:", event);
+//       if (event === "SIGNED_IN") {
+//         const returnUrl = localStorage.getItem('returnUrl');
+//         if (returnUrl) {
+//           console.log("Redirecionando para:", returnUrl);
+//           navigate(returnUrl);
+//           localStorage.removeItem('returnUrl');
+//         } else {
+//           console.log("Redirecionando para: /Dashboard");
+//           navigate("/Expenses");
+//         }
+//       } else {
+//         console.log("Redirecionando para: /");
+//         navigate("/");
+//       }
+//     });
+  
+//     setAuthListener(listener);
+  
+//     return () => {
+//       if (authListener) {
+//         authListener.unsubscribe();
+//       }
+//     };
+//   }, []);
+  
+
+
+
+//   const handleLoginClick = async () => {
+//   console.log("Clicou no botão de login do Google");
+//   const { user, session, error } = await supabase.auth.signIn(
+//     { provider: 'google' },
+//     { redirectTo: 'https://pinetree.pedrosantos.ch/Dashboard' }
+//   );
+//   console.log("Resultado da tentativa de login:", { user, session, error });
+// };
+
+//   return (
+//     <div
+//       className="min-h-screen flex items-center justify-center bg-cover bg-no-repeat backdrop-blur-sm"
+//       style={{ backgroundImage: `url(${auroras})` }}
+//     >
+//       <div className="w-full md:w-1/3 bg-zinc-200 py-4 px-8 pt-10">
+//         <div className="">
+//           <img src={logo} alt="Logo" className="h-32 md:h-36 mt-[-30px] ml-[-10px]" />
+//           <div className="text-teal-700 text-md md:text-2xl">Login</div>
+//         </div>
+//         <p className="text-gray-500 text-xs md:text-sm">Sign in for your expenses</p>
+//         <Auth
+//           supabaseClient={supabase}
+//           theme="default"
+//           providers={['google', 'linkedin', 'discord', 'github']}
+//           appearance={{
+//             theme: ThemeSupa,
+//             variables: {
+//               default: {
+//                 colors: {
+//                   brand: 'teal',
+//                   brandAccent: 'teal',
+//                   dividerBackground: 'teal',
+//                   inputBackground: 'white',
+//                 },
+//               },
+//             },
+//           }} 
+//           className="w-full md:w-1/2 text-xs md:text-xl"
+//           socialLayout="horizontal"
+//           onLogin={() => handleLoginClick()}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import auroras from "../../Images/pineWebp.webp";
 import logo from "../../Images/logo2.png";
@@ -7,12 +99,10 @@ import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 const Login = () => {
-  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const [authListener, setAuthListener] = useState(null);
   
   useEffect(() => {
-    const listener = supabase.auth.onAuthStateChange(async (event, session) => {
+    const handleAuthStateChange = async (event, session) => {
       console.log("Evento de mudança de autenticação:", event);
       if (event === "SIGNED_IN") {
         const returnUrl = localStorage.getItem('returnUrl');
@@ -22,34 +112,29 @@ const Login = () => {
           localStorage.removeItem('returnUrl');
         } else {
           console.log("Redirecionando para: /Dashboard");
-          navigate("/Expenses");
+          navigate("/Dashboard");
         }
       } else {
         console.log("Redirecionando para: /");
         navigate("/");
       }
-    });
-  
-    setAuthListener(listener);
-  
-    return () => {
-      if (authListener) {
-        authListener.unsubscribe();
-      }
     };
-  }, []);
+
+    supabase.auth.onAuthStateChange(handleAuthStateChange);
   
+    // Não há necessidade de unsubscribe, pois não estamos lidando com uma função de retorno assinável
 
-
+  }, [navigate]);
 
   const handleLoginClick = async () => {
-  console.log("Clicou no botão de login do Google");
-  const { user, session, error } = await supabase.auth.signIn(
-    { provider: 'google' },
-    { redirectTo: 'https://pinetree.pedrosantos.ch/Dashboard' }
-  );
-  console.log("Resultado da tentativa de login:", { user, session, error });
-};
+    console.log("Clicou no botão de login do Google");
+    const { user, session, error } = await supabase.auth.signIn(
+      { provider: 'google' },
+      { redirectTo: 'https://pinetree.pedrosantos.ch/Dashboard' }
+    );
+    console.log("Resultado da tentativa de login:", { user, session, error });
+  };
+
 
   return (
     <div
@@ -88,4 +173,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Login
